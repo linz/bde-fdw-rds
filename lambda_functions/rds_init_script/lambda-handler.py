13f2249e50f883173f12dfa597eb54880ceb767a
Jim Gan <jgan@linz.govt.nz>
2023-04-20 13:50:33 +1200

    fix: fetch_size sql syntax
    This option needs to be `ADD`(ed) first. SET is for altering existing value.

--- lambda_functions/rds_init_script/lambda-handler.py
+++ lambda_functions/rds_init_script/lambda-handler.py
@@ -47,7 +47,7 @@ def handler(_event: dict[str, str], _context: LambdaContext) -> None:
                     (bde_host_name,),
                 )
 
-                cur.execute("ALTER SERVER bde_processor OPTIONS (SET fetch_size '100000')")
+                cur.execute("ALTER SERVER bde_processor OPTIONS (ADD fetch_size '100000')")
 
                 cur.execute(
                     "CREATE USER MAPPING IF NOT EXISTS FOR postgres SERVER bde_processor OPTIONS (user %s, password %s)",
